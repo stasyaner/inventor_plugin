59b844787850d8f9f076a80fad847c9a4b41899c
Sidletskiy Stanislav <stasyaner@hotmail.com>
2015-12-07 22:32:09 +0600

    A button's been added.

--- GuitarNeckBuilder/MainViewForm.Designer.cs
+++ GuitarNeckBuilder/MainViewForm.Designer.cs
@@ -28,20 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // Form1
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(30, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // MainViewForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(282, 253);
-            this.Name = "Form1";
+            this.ClientSize = new System.Drawing.Size(566, 361);
+            this.Controls.Add(this.button1);
+            this.Name = "MainViewForm";
             this.Text = "Guitar Neck Builder 3000";
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
 
