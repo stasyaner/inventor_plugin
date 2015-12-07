c670118f2d16ff1231cfa617a1603164302ac244
Sidletskiy Stanislav <stasyaner@hotmail.com>
2015-12-07 22:25:29 +0600

    Form1 to MainViewForm has been renamed include filename.

--- GuitarNeckBuilder/MainViewForm.Designer.cs
+++ GuitarNeckBuilder/MainViewForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace GuitarNeckBuilder
 {
-    partial class Form1
+    partial class MainViewForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Name = "Form1";
+            this.Text = "Guitar Neck Builder 3000";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- GuitarNeckBuilder/MainViewForm.cs
+++ GuitarNeckBuilder/MainViewForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace GuitarNeckBuilder
 {
-    public partial class Form1 : Form
+    public partial class MainViewForm : Form
     {
-        public Form1()
+        public MainViewForm()
         {
             InitializeComponent();
         }
